d16f5aa86577ea8a28c65670ed54cbe328110b0c
Dandi Rizky <dandirizkyy94@gmail.com>
2023-04-07 23:30:43 +0700

    finishing responsive.. done.

--- src/main.js
+++ src/main.js
@@ -61,16 +61,19 @@ async function displayMovieDetails() {
   <h2 class="font-bold text-4xl">${data.title}</h2>
   </div>
   
-  <div class="flex flex-1 lg:mt-10">
-  <img src="${data.image}" class="object-fill h-72 w-52 rounded-2xl mt-1"/>
-  <div class="flex flex-col">
-  <div>
-  <p class="lg:ml-1 lg:justify-evenly flex mx-1 h-min w-auto lg:mt-7 font-bold ">${genre}</p>
+  <div class="flex flex-1 sm:mt-10">
+  <img src="${data.image}" class="object-fill hidden md:flex h-72 w-52 rounded-2xl mt-1"/>
+  <div class="flex flex-col flex-wrap">
+  <div>  
+  <iframe width="375" height="230" class="rounded-2xl sm:hidden mt-16 flex lg:ml-20" src="${data.trailer}"></iframe>
+  <p class="lg:ml-1 lg:justify-evenly flex mx-1 h-min w-auto lg:mt-7 font-bold mt-5 sm:mt-7 ">${genre}</p>
   </div>
-  <p class=" relative text-sm lg:mt-5 lg:ml-3 font-bold w-80">${data.synopsis}</p>
+  <p class=" relative text-lg sm:text-sm mt-5 sm:mt-5 sm:ml-3 font-bold w-80">${data.synopsis}</p>
   <p class="lg:ml-3 mt-20">IMBD Rating ⭐${data.rating}/10</p>
-  </div>  
-  <iframe width="590" height="320" class="rounded-2xl lg:ml-20" src="${data.trailer}"></iframe>
+  </div>
+  <div class="sm:-mt-4">  
+  <iframe width="590" height="320" class="rounded-2xl hidden sm:flex lg:ml-20" src="${data.trailer}"></iframe>
+  </div>
   </div>
   
   
